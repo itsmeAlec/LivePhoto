80c4a1575bdb492edfd50e8111e77b81f63ba282
itsmeAlec <alecnagal@gmail.com>
2025-03-17 23:29:43 -0700

    Initial Commit

---
+++ 3year/_yearApp.swift
@@ -0,0 +1,17 @@
+//
+//  _yearApp.swift
+//  3year
+//
+//  Created by Alec Nagal on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct _yearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3yearTests/_yearTests.swift
@@ -0,0 +1,17 @@
+//
+//  _yearTests.swift
+//  3yearTests
+//
+//  Created by Alec Nagal on 3/17/25.
+//
+
+import Testing
+@testable import _year
+
+struct _yearTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
